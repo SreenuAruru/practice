9cdeaea793964803bcf77bc10d87da5a7e1ceb96
SreenuAruru <arurusreenu4@gmail.com>
2023-07-28 23:10:18 +0530

    first stage

---
+++ pratice-git/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Practice from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <Practice />
+    </div>
+  );
+}
+
+export default App;
---
+++ pratice-git/src/components/index.js
@@ -0,0 +1,5 @@
+function Practice() {
+  return <p>testing first git repository-----1</p>;
+}
+
+export default Practice;
